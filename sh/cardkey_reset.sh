2ea530a3fe47f7ed91492c8ecd6b00694f6b0198
strymj <yamaji.shoutarou@gmail.com>
2017-02-24 15:25:42 +0900

    ver2.0

--- sh/cardkey_reset.sh
+++ sh/cardkey_reset.sh
@@ -1,17 +1,29 @@
 #!/bin/sh
 
 LOOP=0
+RESET_TIME="15:24"
 
-while [ ${LOOP} -ge 0 ]
+
+while :
 do
-	LOOP=`expr $LOOP + 1`
-	echo "<Restarted ${LOOP}>"
 	gnome-terminal --geometry 40x11+1200+400 -e 'bash -c "sh ~/catkin_ws/src/cardkey/sh/cardkey.sh"'
-	sleep 3600
-	echo "Restart ID-program in 10 seconds."
+	
+	echo "Running ${LOOP} days.\n"
+	LOOP=$(( LOOP + 1 ))
+
+	sleep 60
+
+	NOW_TIME=`date '+%H:%M'`
+	while [ ${RESET_TIME} != ${NOW_TIME} ]
+	do
+		sleep 60
+		NOW_TIME=`date '+%H:%M'`
+	done
+	
+	echo "Restart cardkey program after 10 seconds."
 	sleep 10
 	ps -e | grep roslaunch | grep -v grep | awk '{ print "kill -2", $1 }' | sh
-	sleep 5
+	echo "Restarted.\n\n\n"
 done
 
 exit 0
